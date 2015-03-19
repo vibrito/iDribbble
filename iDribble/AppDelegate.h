5def8dbc408b0732c6f833c8bdc2fb3b86984bd6
Vinicius Brito <vibrito@gmail.com>
2015-03-19 17:06:12 -0300

    Initial Commit

---
+++ iDribble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDribble
+//
+//  Created by Vinicius Brito on 19/03/15.
+//  Copyright (c) 2015 Vinicius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iDribble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDribble
+//
+//  Created by Vinicius Brito on 19/03/15.
+//  Copyright (c) 2015 Vinicius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
